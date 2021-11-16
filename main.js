ae7d8c7154090c169abb20f2e551cb70652f1c0a
Caio Felipe <devcaiofelipe15@gmail.com>
2021-11-16 17:57:26 -0300

    fix(desestruturacao): usar desestruracao para evitar repeticao

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ import fs from 'fs';
 for(const app of appList) {
     const basePath = process.cwd();
     const configPath = basePath + `/apps/${app}/config.json`;
-    const config = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
-    
+    const { appName, domain, color, appType, packageName }  = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
+
     try {
-        console.log(`Aplicativo ${config.appName} iniciado com sucesso, esse processo dura em média 5 minutos. Aguarde!`);
-        const application = new App(config.appName, config.domain, config.color, config.appType, config.packageName);
+        console.log(`Aplicativo ${appName} iniciado com sucesso, esse processo dura em média 5 minutos. Aguarde!`);
+        const application = new App(appName, domain, color, appType, packageName);
         Terminal.undoAll();
         application.makeApp();
         Terminal.gradlewClean();
         Terminal.generateApp(application.getAppType);
         application.getOutput()
         Terminal.undoAll();
-        console.log(`Aplicativo ${config.appName} finalizado com sucesso`);
+        console.log(`Aplicativo ${appName} finalizado com sucesso`);
     }catch(err) {
         console.log('[ERROR]', err);
         Terminal.undoAll();
