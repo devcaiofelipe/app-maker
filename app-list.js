b8837a41ea6d7bfca9d28a09733fc9ceb9b21422
Caio Felipe <devcaiofelipe15@gmail.com>
2021-12-01 09:42:38 -0300

    fix: retirar dedexexpress que estava duplicado

--- app-list.js
+++ app-list.js
@@ -16,7 +16,6 @@ export const appList = [
     'coperboy',
     'dcomotoboy',
     'dedexexpress',
-    'dedexexpress',
     'deliverymanausexpress',
     'diferencialmotoexpress',
     'direcaoexpress',
